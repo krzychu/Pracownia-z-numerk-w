32cdf13f4ac6c320fd5be84a8012cf08199d9095
krzychu-old-lap <krzychuch@gmail.com>
2011-12-10 10:13:14 +0100

    klasy też można automatycznie eksportować

--- zadanie3/python_binding_experiment/test.cpp
+++ zadanie3/python_binding_experiment/test.cpp
@@ -4,7 +4,30 @@ const char * hello(){
   return "hello\n";
 }
 
+class Foo{
+  public:
+    Foo(int initial_value){
+      m_counter = initial_value;
+    }
+
+    void inc(){
+      m_counter++;
+    }
+
+    int get(){
+      return m_counter;
+    }
+
+  private:
+    int m_counter;
+};
+
+
 BOOST_PYTHON_MODULE(test){
   using namespace boost::python;
   def("hello", hello);
+  class_<Foo>("Foo", init<int>())
+    .def("get", &Foo::get)
+    .def("inc", &Foo::inc);
+
 }
